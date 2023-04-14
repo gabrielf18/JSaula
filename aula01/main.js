5cf1d0e7bad152237cb6ec36d04d9667348b2e16
Gabriel <gabrielfserrano0@gmail.com>
2023-04-13 21:25:28 -0300

    First seen

---
+++ aula01/main.js
@@ -0,0 +1,10 @@
+var nome = 'Gabriel'
+let idade = 21
+const cidade = 'Caraguatatuba'
+
+var nome = 'Gabriel Ferreira'
+idade = 22
+
+
+console.log(nome + ' tem ' + idade + ' anos e mora em ' + cidade);
+console.log(`${nome} tem ${idade} anos e mora em ${cidade}`)
